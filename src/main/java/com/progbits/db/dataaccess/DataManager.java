b8aef1062b792066ac1258849192b47be03a78a8
Scott Carr <kscarr73@gmail.com>
2024-07-09 18:56:01 -0500

    Initial Checkin for Data Manager

---
+++ src/main/java/com/progbits/db/dataaccess/DataManager.java
@@ -0,0 +1,414 @@
+package com.progbits.db.dataaccess;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.progbits.api.config.ConfigProvider;
+import com.progbits.api.exception.ApiClassNotFoundException;
+import com.progbits.api.exception.ApiException;
+import com.progbits.api.model.ApiObject;
+import com.progbits.api.parser.YamlObjectParser;
+import com.progbits.db.SsDbObjects;
+import com.progbits.db.SsDbUtils;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+/**
+ * DataManager implementation using HikariCP and ApiObjects
+ * 
+ * @author kscarr73
+ */
+public class DataManager {
+    private static final Logger log = LoggerFactory.getLogger(DataManager.class);
+
+    private static DataManager instance;
+    private static ReentrantLock lock;
+    private final CountDownLatch configured = new CountDownLatch(1);
+
+    public synchronized static DataManager getInstance() {
+        if (lock == null) {
+            lock = new ReentrantLock();
+        }
+
+        if (instance == null) {
+            lock.lock();
+
+            try {
+            instance = new DataManager();
+
+            instance.pullConfigs();
+
+            instance.configure();
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        try {
+            instance.configured.await();
+        } catch (InterruptedException ex) {
+            // nothing to report
+        }
+
+        return instance;
+    }
+
+    public synchronized void configure() {
+        for (var entry : dbConfigs.keySet()) {
+            ds.put(entry, setupPool(entry));
+        }
+        
+        loadSqlEntries();
+
+        configured.countDown();
+    }
+    
+    private void pullConfigs() {
+        for (var propName : ConfigProvider.getInstance().getConfig().entrySet()) {
+            if (propName.getKey().startsWith(DB_HOST)) {
+                if (DB_HOST.equals(propName.getKey())) {
+                    dbConfigs.setObject(DEFAULT, pullDbConfig(null));
+                } else {
+                    String keyName = propName.getKey().substring(8);
+
+                    dbConfigs.setObject(keyName, pullDbConfig(keyName));
+                }
+            }
+        }
+    }
+
+    private ApiObject pullDbConfig(String name) {
+        ApiObject objRet = new ApiObject();
+
+        String lclName;
+
+        if (name == null) {
+            lclName = "";
+        } else {
+            lclName = "_" + name;
+        }
+
+        objRet.setString(DB_HOST, config.getStringProperty(DB_HOST + lclName));
+        objRet.setString(DB_DRIVER, config.getStringProperty(DB_DRIVER + lclName));
+
+//        if (!objRet.isSet(DB_DRIVER)) {
+//            objRet.setString(DB_DRIVER, "com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//        }
+        objRet.setString(DB_NAME, config.getStringProperty(DB_NAME + lclName));
+        objRet.setString(DB_USER, config.getStringProperty(DB_USER + lclName));
+        objRet.setString(DB_PASSWORD, config.getStringProperty(DB_PASSWORD + lclName));
+        objRet.setString(DB_TESTQUERY, config.getStringProperty(DB_TESTQUERY + lclName));
+        objRet.setString(DB_MAXCONNECTIONS, config.getStringProperty(DB_MAXCONNECTIONS + lclName));
+
+        log.info("Db Maestro User: {}", config.getStringProperty("MAESTRO_POSTGRES_MEMBERCONTACTS_USERNAME_ADMIN"));
+        return objRet;
+    }
+
+    private void loadSqlEntries() {
+        try (InputStream is = getClass().getResourceAsStream("/db/sql.yml")) {
+            YamlObjectParser parser = new YamlObjectParser(true);
+            
+            sqlEntries = parser.parseSingle(new InputStreamReader(is));
+        } catch (ApiException | ApiClassNotFoundException appx) {
+            log.info("DB SQL Entries Read Failed");
+        } catch (IOException iex) {
+            log.info("DB SQL Entries Not Found");
+        }
+    }
+    
+    public static final String DEFAULT = "default";
+
+    private static final String DB_HOST = "DB_HOST";
+    private static final String DB_DRIVER = "DB_DRIVER";
+    private static final String DB_NAME = "DB_NAME";
+    private static final String DB_USER = "DB_USER";
+    private static final String DB_PASSWORD = "DB_PASSWORD";
+    private static final String DB_TESTQUERY = "DB_TESTQUERY";
+    private static final String DB_MAXCONNECTIONS = "DB_MAXCONNECTIONS";
+
+    private static ConfigProvider config = ConfigProvider.getInstance();
+
+    private Map<String, HikariDataSource> ds = new HashMap<>();
+
+    private ApiObject sqlEntries = null;
+    
+    private ApiObject dbConfigs = new ApiObject();
+
+    private HikariDataSource setupPool(String name) {
+        ApiObject dbData = dbConfigs.getObject(name);
+
+        HikariConfig dbconfig = new HikariConfig();
+
+        if (!dbData.getString(DB_HOST).startsWith("jdbc:") || (dbData.getString(DB_DRIVER) != null && !dbData.getString(DB_DRIVER).isEmpty())) {
+            dbconfig.setDriverClassName(dbData.getString(DB_DRIVER));
+        }
+
+        try {
+            if (dbData.isSet(DB_DRIVER) && dbData.getString(DB_DRIVER).toLowerCase().contains("sqlserver")) {
+                String strUrl = "jdbc:sqlserver://" + dbData.getString(DB_HOST) + ";databaseName=" + dbData.getString(DB_NAME) + ";user=" + dbData.getString(DB_USER) + ";";
+                dbconfig.setJdbcUrl(strUrl);
+                dbconfig.setPassword(dbData.getString(DB_PASSWORD));
+            } else if (!dbData.getString(DB_HOST).startsWith("jdbc:")
+                    && dbData.isSet(DB_DRIVER)
+                    && dbData.getString(DB_DRIVER).toLowerCase().contains("jtds")) {
+                dbconfig.setJdbcUrl("jdbc:jtds:sqlserver://" + dbData.getString(DB_HOST) + ";DatabaseName=" + dbData.getString(DB_NAME));
+                dbconfig.setUsername(dbData.getString(DB_USER));
+                dbconfig.setPassword(dbData.getString(DB_PASSWORD));
+            } else {
+                log.info("Db Settings: {} DbName: {} User: {}", dbData.getString(DB_HOST)
+                    , dbData.getString(DB_NAME), dbData.getString(DB_USER));
+                
+                if (dbData.getString(DB_HOST) != null && dbData.getString(DB_HOST).endsWith("/")) {
+                    dbconfig.setJdbcUrl(dbData.getString(DB_HOST) + dbData.getString(DB_NAME));
+                } else {
+                    dbconfig.setJdbcUrl(dbData.getString(DB_HOST) + "/" + dbData.getString(DB_NAME));
+                }
+                
+                dbconfig.setUsername(dbData.getString(DB_USER));
+                dbconfig.setPassword(dbData.getString(DB_PASSWORD));
+            }
+
+            if (dbData.getString(DB_TESTQUERY) != null) {
+                dbconfig.setConnectionTestQuery(dbData.getString(DB_TESTQUERY));
+            }
+
+            if (dbData.isSet(DB_MAXCONNECTIONS)) {
+                dbconfig.setMaximumPoolSize(Integer.parseInt(dbData.getString(DB_MAXCONNECTIONS)));
+            }
+            
+            return new HikariDataSource(dbconfig);
+        } catch (Exception ex) {
+            log.error("Database Connection Failed: " + ex.getMessage(), ex);
+        }
+
+        return null;
+    }
+
+    public boolean getStatus() {
+        boolean bRet = false;
+
+        for (var entry : ds.entrySet()) {
+            if (entry.getValue() != null) {
+                bRet = entry.getValue().isRunning();
+            } else {
+                bRet = false;
+            }
+
+            if (!bRet) {
+                break;
+            }
+        }
+
+        return bRet;
+    }
+
+    public String getSqlEntry(String name) {
+        if (sqlEntries != null) {
+            return sqlEntries.getString(name);
+        } else {
+            return null;
+        }
+    }
+    
+    public ApiObject getConfig() {
+        return dbConfigs;
+    }
+
+    public void setConfig(ApiObject objConfig) {
+        dbConfigs = objConfig;
+    }
+
+    public HikariDataSource getDataSource() {
+        return getDataSource(DEFAULT);
+    }
+
+    public HikariDataSource getDataSource(String name) {
+        return ds.get(name);
+    }
+
+    public Connection getConnection() throws ApiException {
+        return getConnection(DEFAULT);
+    }
+
+    public Connection getConnection(String name) throws ApiException {
+        try {
+            if (ds == null) {
+                configure();
+            }
+
+            return ds.get(name).getConnection();
+        } catch (SQLException sqx) {
+            throw new ApiException(511, sqx.getMessage());
+        }
+    }
+
+    public ApiObject getTable(String tableName, ApiObject searchObj) throws ApiException {
+        return getTable(DEFAULT, tableName, searchObj);
+    }
+
+    public ApiObject getTable(String dbName, String tableName, ApiObject searchObj) throws ApiException {
+        try ( Connection conn = ds.get(dbName).getConnection()) {
+            searchObj.setString("tableName", tableName);
+
+            return SsDbObjects.find(conn, searchObj);
+        } catch (SQLException sqx) {
+            throw new ApiException(500, sqx.getMessage(), sqx);
+        } catch (Exception ex) {
+            throw new ApiException(400, ex.getMessage(), ex);
+        }
+    }
+
+    public ApiObject saveIntegerKey(String dbName, String tableName, String id, ApiObject objSave) throws ApiException {
+        try ( Connection conn = ds.get(dbName).getConnection()) {
+            return SsDbObjects.upsertWithIntegerKey(conn, tableName, id, objSave);
+        } catch (SQLException sqx) {
+            throw new ApiException(500, sqx.getMessage(), sqx);
+        } catch (Exception ex) {
+            if (ex.getMessage() != null) {
+                if (ex.getMessage().contains("duplicate key")) {
+                    throw new ApiException(400, "Duplicate Record", ex);
+                } else {
+                    throw new ApiException(400, ex.getMessage(), ex);
+                }
+            }
+            throw new ApiException(400, ex.getMessage(), ex);
+        }
+    }
+
+    public ApiObject saveStringKey(String dbName, String tableName, String id, ApiObject objSave) throws ApiException {
+        try ( Connection conn = ds.get(dbName).getConnection()) {
+            return SsDbObjects.upsertWithStringKey(conn, tableName, id, objSave);
+        } catch (SQLException sqx) {
+            throw new ApiException(500, sqx.getMessage(), sqx);
+        } catch (Exception ex) {
+            throw new ApiException(400, ex.getMessage(), ex);
+        }
+    }
+
+    /**
+     * Delete a Row from a Database
+     * 
+     * @param dbName The configured Db name
+     * @param tableName The table name to delete from
+     * @param fieldName The field that is the ID
+     * @param value The ID to delete
+     * @return True if the SQL ran successfully.  Not if the ID didn't exist, an error is NOT thrown.
+     * 
+     * @throws ApiException 
+     */
+    public boolean deleteId(String dbName, String tableName, String fieldName, Object value) throws ApiException {
+        try ( Connection conn = ds.get(dbName).getConnection()) {
+            String strSql = "DELETE FROM " + tableName + " WHERE " + fieldName + "=?";
+
+            List<Object> args = new ArrayList<>();
+
+            args.add(value);
+
+            SsDbUtils.update(conn, strSql, args.toArray());
+
+            return true;
+        } catch (SQLException sqx) {
+            throw new ApiException(500, sqx.getMessage(), sqx);
+        } catch (Exception ex) {
+            throw new ApiException(400, ex.getMessage(), ex);
+        }
+    }
+    
+    /**
+     * Run SQL with field replacement
+     * 
+     * @param sql SQL with :{field} replaceable values.  
+     * @param search Values in SQL string with :{field} MUST exist in search
+     * @return ApiObject with a root list of returned rows
+     * @throws ApiException 
+     */
+    public ApiObject getSQLRows(String dbName, String sql, ApiObject search) throws ApiException {
+        try (Connection conn = getConnection(dbName)) {
+            return SsDbUtils.querySqlAsApiObject(conn, sql, search);
+        } catch (SQLException sqx) {
+            throw new ApiException(500, sqx.getMessage(), sqx);
+        } catch (Exception ex) {
+            throw new ApiException(400, ex.getMessage(), ex);
+        }
+    }
+    
+    /**
+     * Run SQL with field replacement
+     * 
+     * @param sql SQL with :{field} replaceable values.  
+     * @param search Values in SQL string with :{field} MUST exist in search
+     * @return ApiObject with a root list of returned rows
+     * @throws ApiException 
+     */
+    public ApiObject getSQLRows(String dbName, String sql, Object[] search) throws ApiException {
+        try (Connection conn = getConnection(dbName)) {
+            return SsDbUtils.querySqlAsApiObject(conn, sql, search);
+        } catch (SQLException sqx) {
+            throw new ApiException(500, sqx.getMessage(), sqx);
+        } catch (Exception ex) {
+            throw new ApiException(400, ex.getMessage(), ex);
+        }
+    }
+    
+    public ApiObject getSQLFirstRow(String dbName, String sql, ApiObject search) throws ApiException {
+        ApiObject objResp = getSQLRows(dbName, sql, search);
+        
+        if (objResp.isSet("root")) {
+            return objResp.getObject("root[0]");
+        } else {
+            return null;
+        }
+    }
+    
+    public ApiObject getSQLFirstRow(String dbName, String sql, Object[] search) throws ApiException {
+        ApiObject objResp = getSQLRows(dbName, sql, search);
+        
+        if (objResp.isSet("root")) {
+            return objResp.getObject("root[0]");
+        } else {
+            return null;
+        }
+    }
+    
+    public Integer executeSQL(String dbName, String sql, ApiObject search) throws ApiException {
+        try (Connection conn = getConnection(dbName)) {
+            return SsDbUtils.updateObjectWithCount(conn, sql, search);
+        } catch (SQLException sqx) {
+            throw new ApiException(500, sqx.getMessage(), sqx);
+        } catch (Exception ex) {
+            throw new ApiException(400, ex.getMessage(), ex);
+        }
+    }
+    
+    public Integer executeSQL(String dbName, String sql, Object[] args) throws ApiException {
+        try (Connection conn = getConnection(dbName)) {
+            return SsDbUtils.updateWithCount(conn, sql, args);
+        } catch (SQLException sqx) {
+            throw new ApiException(500, sqx.getMessage(), sqx);
+        } catch (Exception ex) {
+            throw new ApiException(400, ex.getMessage(), ex);
+        }
+    }
+    
+    public String getSqlString(String dbName, String sql, Object[] args) throws ApiException {
+        try (Connection conn = getConnection(dbName)) {
+            return SsDbUtils.queryForString(conn, sql, args);
+        } catch (SQLException sqx) {
+            throw new ApiException(500, sqx.getMessage(), sqx);
+        } catch (Exception ex) {
+            throw new ApiException(400, ex.getMessage(), ex);
+        }
+    }
+}
